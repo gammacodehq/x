a8686c1f64ea8e6e3b25d0ba74b9496047b6fd41
notTGY <65339965+notTGY@users.noreply.github.com>
2025-08-06 12:09:50 +0300

    Create EscapeStupid.go

---
+++ safe_tg/EscapeStupid.go
@@ -0,0 +1,21 @@
+package safe_tg
+
+import (
+ "fmt"
+ "strings"
+)
+
+// Stupid (but works) escape: just prepend \ to every special symbol.
+func EscapeStupid(text string) string {
+ // All characters that need escaping in Telegram Markdown.
+ special := `\\*_[]()~>#+-=|{}.!` // note: backslash included once
+
+ var out strings.Builder
+ for _, r := range text {
+  if strings.ContainsRune(special, r) {
+   out.WriteRune('\\')
+  }
+  out.WriteRune(r)
+ }
+ return out.String()
+}
